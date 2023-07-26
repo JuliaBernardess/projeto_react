bd0a307839d65f68579a7a9ead50ba35e18cc534
Julia Bernardes <129619510+JuliaBernardess@users.noreply.github.com>
2023-07-25 21:24:12 -0300

    rotas

--- src/App.tsx
+++ src/App.tsx
@@ -36,14 +36,22 @@ export default App;*/
 export default App;*/
 
 
+
 import './App.css';
 import Home from './paginas/home/Home';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/Login/Login';
 function App() {
   return (
     <>
-    <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-);
+  );
 }
 export default App;
