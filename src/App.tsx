a722de5da86b94049799393ae79cd955e0e6bd35
Julia Bernardes <129619510+JuliaBernardess@users.noreply.github.com>
2023-07-26 17:47:07 -0300

    certo

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,44 @@
 
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Home from './paginas/home/Home';
 
+interface User {
+  id: number;
+  name: string;
+}
 
+function App() {
 
+  const [users, setUsers] = useState<User[]>([]);
 
+  useEffect(() => {
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
- /*function App() {
-  const [valor, setValor] = useState(0);
-
-  function handleClick() {
-    setValor(valor + 1);
-  }
   return (
-    //  <Home  title="Componente Home"
-    //         description="Este é um componente Home que recebe props."/>
     <div>
-      <p>O valor é: {valor}</p>
-      <button onClick={handleClick}>Adicionar 1</button>
+      <h1>Lista de usuários</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;*/
-
+export default App;
 
 
 /*function App() {
- 
-  return (
-    <>
-      <Home/>
-    </>
-  );
-}
-
-export default App;*/
-
-import './App.css';
-import Home from './paginas/home/Home';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './paginas/Login/Login';
-
-function App() {
   return (
     <>
     <BrowserRouter>
@@ -59,4 +55,4 @@ function App() {
     </>
 );
 }
-export default App;
+export default App;*/
