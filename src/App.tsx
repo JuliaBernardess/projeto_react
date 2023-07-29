b28d809b422f4dee9fb401d102071899efb0c5e8
Julia Bernardes <129619510+JuliaBernardess@users.noreply.github.com>
2023-07-28 23:00:14 -0300

    Consumo do Back End - CRUD de Temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+
 import './App.css';
 
 import Navbar from './components/navbar/Navbar';
@@ -9,6 +10,8 @@ import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 
 function App() {
@@ -19,11 +22,14 @@ function App() {
           <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-            <Route path="/" element={<Login />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
               <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -20,14 +20,14 @@ function Navbar() {
 
   return (
     <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+     <div className='w-full bg-gradient-to-r from-cyan-600 bg-blue-600 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
           <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
             <div className='flex gap-4'>
             <div className='hover:underline'>Postagens</div>
             <Link to='/temas' className='hover:underline'>Temas</Link>
-              <div className='hover:underline'>Cadastrar tema</div>
+            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
               <div className='hover:underline'>Perfil</div>
               <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
              
